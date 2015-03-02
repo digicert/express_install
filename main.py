3ccdde91797f8714ee7ec3de43364977f69f4f79
Joe Gerew <joe.gerew@digicert.com>
2015-03-02 15:49:16 -0700

    Added creation of new secure virtual host and file formatting

--- main.py
+++ main.py
@@ -32,6 +32,8 @@ def run():
                           help="I need a host to update")
     parser_b.add_argument("--cert", action="store",
                           help="I need the path to the cert for the configuration file")
+    parser_b.add_argument("--key", action="store",
+                          help="I need the path to the key for the configuration file")
     parser_b.add_argument("--chain", action="store",
                           help="I need the cert chain for the configuration file")
     parser_b.set_defaults(func=parse_apache)
@@ -78,9 +80,9 @@ def restart_apache(args):
 
 def parse_apache(args):
     print "my job is to parse the apache configuration file and store a backup and update the ssl config"
-    if args.host and args.cert and args.chain:
+    if args.host and args.cert and args.key and args.chain:
         try:
-            apache_parser = BaseParser(args.host, args.cert, args.chain)
+            apache_parser = BaseParser(args.host, args.cert, args.key, args.chain)
             apache_parser.load_apache_configs()
             virtual_host = apache_parser.get_vhost_path_by_domain()
             apache_parser.set_certificate_directives(virtual_host)
--- parsers/base.py
+++ parsers/base.py
@@ -1,6 +1,7 @@
 import augeas
 import os
 import shutil
+import re
 from collections import OrderedDict
 
 
@@ -16,32 +17,33 @@ class ParserException(Exception):
 class BaseParser(object):
     """docstring for BaseParser"""
 
-    def __init__(self, domain, cert_path, chain_path, aug=None):
+    def __init__(self, domain, cert_path, key_path, chain_path, aug=None):
         self.domain = domain
 
         # verify that the files exist and are readable by the user
-        if not os.path.isfile(cert_path):
-            raise ParserException("{0} could not be found on the filesystem".format(cert_path))
-
-        if not os.path.isfile(chain_path):
-            raise ParserException("{0} could not be found on the filesystem".format(chain_path))
-
-        cert_path_perm = int(oct(os.stat(cert_path).st_mode)[-3:])
-        if cert_path_perm != 755:
-            raise ParserException("{0} does not have the necessary permissions set (755 required, {1} set)".format(cert_path, cert_path_perm))
-
-        chain_path_perm = int(oct(os.stat(chain_path).st_mode)[-3:])
-        if chain_path_perm != 755:
-            raise ParserException("{0} does not have the necessary permissions set (755 required, {1} set)".format(chain_path, chain_path_perm))
+        self._verify_file(cert_path)
+        self._verify_file(key_path)
+        self._verify_file(chain_path)
 
         self.directives = OrderedDict()
+        self.directives['SSLEngine'] = "on"
         self.directives['SSLCertificateFile'] = cert_path
+        self.directives['SSLCertificateKeyFile'] = key_path
         self.directives['SSLCertificateChainFile'] = chain_path
 
         if not aug:
             my_flags = augeas.Augeas.NONE | augeas.Augeas.NO_MODL_AUTOLOAD
             aug = augeas.Augeas()  # flags=my_flags)
         self.aug = aug
+
+    @staticmethod
+    def _verify_file(file_path):
+        if not os.path.isfile(file_path):
+            raise ParserException("{0} could not be found on the filesystem".format(file_path))
+
+        file_perm = int(oct(os.stat(file_path).st_mode)[-3:])
+        if file_perm != 755:
+            raise ParserException("{0} does not have the necessary permissions set (755 required, {1} set)".format(file_path, file_perm))
 
     def load_apache_configs(self):
         try:
@@ -76,48 +78,160 @@ class BaseParser(object):
         matches = self.aug.match("/files/etc/apache2/sites-available/*")
         for host_file in matches:
             if '~previous' not in host_file:
-                print host_file
                 vhosts = self.aug.match(host_file + "/VirtualHost")
-                if not vhosts:
-                    vhosts = self.aug.match(host_file + "/*/VirtualHost")
-                for vhost in vhosts:
-                    print self.aug.get(vhost + "/arg")
-                    if '443' in self.aug.get(vhost + "/arg"):
-                        check_matches = self.aug.match(vhost + '/directive')
-                        if check_matches:
-                            for check in check_matches:
-                                if self.aug.get(check) == 'ServerName' and self.aug.get(check + "/arg") is not None \
-                                        and self.aug.get(check + "/arg") == self.domain:
-                                    return vhost
-                        else:
-                            check_matches = self.aug.match(vhost + '/directive')
-                            for check in check_matches:
-                                if self.aug.get(check) == 'ServerName' and self.aug.get(check + "/arg") is not None \
-                                        and self.aug.get(check + "/arg") == self.domain:
-                                    return vhost
+                vhosts += self.aug.match(host_file + "/*/VirtualHost")
+
+                vhost = self._get_vhost_path_by_domain_and_port(vhosts, '443')
+                if not vhost:
+                    vhost = self._get_vhost_path_by_domain_and_port(vhosts, '80')
+
+                    if vhost:
+                        # we didn't find an existing 443 virtual host but found one on 80
+                        # create a new virtual host for 443 based on 80
+                        vhost = self._create_secure_vhost(vhost)
+
+                # return as soon as we have a vhost
+                if vhost:
+                    return vhost
+
+    def _get_vhost_path_by_domain_and_port(self, vhosts, port):
+        for vhost in vhosts:
+            if port in self.aug.get(vhost + "/arg"):
+                check_matches = self.aug.match(vhost + '/directive')
+                if check_matches:
+                    for check in check_matches:
+                        if self.aug.get(check) == 'ServerName' and self.aug.get(check + "/arg") is not None \
+                                and self.aug.get(check + "/arg") == self.domain:
+                            return vhost
+                else:
+                    check_matches = self.aug.match(vhost + '/directive')
+                    for check in check_matches:
+                        if self.aug.get(check) == 'ServerName' and self.aug.get(check + "/arg") is not None \
+                                and self.aug.get(check + "/arg") == self.domain:
+                            return vhost
+
+    def _create_secure_vhost(self, vhost):
+        secure_vhost = None
+        host_file = vhost[0:vhost.index("VirtualHost")-1]
+
+        # create a map of the insecure vhost's configuration
+        vhost_map = list()
+        self._create_map_from_vhost(vhost, vhost_map)
+
+        # check if there is an IfModule for mod_ssl.c, if not create it
+        if_module = None
+        check_matches = self.aug.match(host_file + "/IfModule")
+        if check_matches:
+            for check in check_matches:
+                if self.aug.get(check + "/arg") == "mod_ssl.c":
+                    if_module = check
+
+        if not if_module:
+            self.aug.set(host_file + "/IfModule[last()+1]/arg", "mod_ssl.c")
+            if_modules = self.aug.match("{0}/*[self::IfModule/arg='mod_ssl.c']".format(host_file))
+            if len(if_modules) > 0:
+                if_module = if_modules[0]
+            else:
+                raise ParserException(
+                    "An error occurred while creating IfModule mod_ssl.c for {0}.\n{1}".format(self.domain, e.message),
+                    self.directives)
+
+        # create a new secure vhost
+        vhost_name = self.aug.get(vhost + "/arg")
+        vhost_name = vhost_name[0:vhost_name.index(":")] + ":443"
+        self.aug.set(if_module + "/VirtualHost[last()+1]/arg", vhost_name)
+        vhosts = self.aug.match("{0}/IfModule/*[self::VirtualHost/arg='{1}']".format(host_file, vhost_name))
+        for vhost in vhosts:
+            secure_vhost = vhost
+
+            # write the insecure vhost configuration into the new secure vhost
+            self._create_vhost_from_map(secure_vhost, vhost_map)
+
+        self.check_for_parsing_errors()
+
+        return secure_vhost
+
+    def _create_map_from_vhost(self, path, vhost_map, text=""):
+        # recurse through the directives and sub-groups to generate a map
+        check_matches = self.aug.match(path + '/*')
+        if check_matches:
+            for check in check_matches:
+                values = list()
+                # get the type of configuration
+                config_type = check[len(path)+1:]
+                config_name = self.aug.get(check)
+                config_value = self.aug.get(check + "/arg")
+
+                if "arg" not in config_type and "#comment" not in config_type:
+                    if not config_value:
+                        check_matches1 = self.aug.match("{0}/{1}/arg".format(path, config_type))
+                        for check1 in check_matches1:
+                            values.append(self.aug.get(check1))
+                            if config_value:
+                                config_value += " {0}".format(self.aug.get(check1))
+                            else:
+                                config_value = self.aug.get(check1)
+                    else:
+                        values.append(config_value)
+
+                    if not config_name:
+                        sub_map = list()
+                        vhost_map.append({'type': config_type, 'name': None, 'values': values, 'sub_group': sub_map})
+                        self._create_map_from_vhost(path + "/" + config_type, sub_map, "{0}\t".format(text))
+                    else:
+                        vhost_map.append({'type': config_type, 'name': config_name, 'values': values, 'sub_group': None})
+
+    def _create_vhost_from_map(self, path, vhost_map, text=""):
+        # recurse through the map and write the new vhost
+        for entry in vhost_map:
+            config_type = entry['type']
+            config_name = entry['name']
+            config_values = entry['values']
+            config_sub = entry['sub_group']
+
+            value = None
+            for v in config_values:
+                if not value:
+                    value = v
+                else:
+                    value += " {0}".format(v)
+
+            self.aug.set("{0}/{1}".format(path, config_type), config_name)
+            if len(config_values) > 1:
+                i = 1
+                for value in config_values:
+                    self.aug.set("{0}/{1}/arg[{2}]".format(path, config_type, i), value)
+                    i += 1
+            else:
+                self.aug.set("{0}/{1}/arg".format(path, config_type), value)
+
+            if not config_name and config_type and config_sub:
+                # this is a sub-group
+                # sub_groups = self.aug.match("{0}/{1}[last()]".format(path, config_type))
+                sub_groups = self.aug.match("{0}/{1}".format(path, config_type))
+                for sub_group in sub_groups:
+                    self._create_vhost_from_map(sub_group, config_sub, "{0}\t".format(text))
 
     def set_certificate_directives(self, vhost_path):
         try:
-
             if not vhost_path:
                 raise Exception("Virtual Host was not found for {0}".format(self.domain))
 
-            # back up the configutation file
-            conf_file = vhost_path[6:vhost_path.index("IfModule")-1]
-            shutil.copy(conf_file, "{0}~previous".format(conf_file))
+            # back up the configuration file
+            host_file = vhost_path[6:vhost_path.index("IfModule")-1]
+            shutil.copy(host_file, "{0}~previous".format(host_file))
 
-            updates = 0
             errors = []
             for directive in self.directives:
                 val = self.aug.match("{0}/*[self::directive='{1}']".format(vhost_path, directive))
                 if val:
                     self.aug.set("{0}/*[self::directive='{1}']/arg".format(vhost_path, directive), self.directives[directive])
-                    updates += 1
                 else:
-                    errors.append("Could not find " + directive)
+                    self.aug.set(vhost_path + "/directive[last()+1]", directive)
+                    self.aug.set(vhost_path + "/directive[last()]/arg", self.directives[directive])
 
-            if len(errors) > 0 or updates < len(self.directives):
-                error_msg = "Could not update all directives ({0}):\n".format(updates)
+            if len(errors):
+                error_msg = "Could not update all directives:\n"
                 for error in errors:
                     error_msg = "{0}\t{1}\n".format(error_msg, error)
                 raise Exception(error_msg)
@@ -141,6 +255,41 @@ class BaseParser(object):
                 raise Exception(error_msg)
 
         except Exception, e:
+            self.check_for_parsing_errors()
             raise ParserException(
                 "An error occurred while updating the Virtual Host for {0}.\n{1}".format(self.domain, e.message),
                 self.directives)
+
+        # format the file:
+        self._format_config_file(host_file)
+
+        # verify that augeas can still load the changed file
+        self.load_apache_configs()
+
+
+    def _format_config_file(self, host_file):
+        try:
+            lines = list()
+            with open(host_file) as f:
+                lines = f.read().splitlines()
+
+            f = open(host_file, 'w+')
+
+            tabs = ""
+            for line in lines:
+                line = line.lstrip()
+                if re.match("^<(\w+)", line):
+                    f.write("{0}{1}\n".format(tabs, line))
+                    tabs += "\t"
+                else:
+                    if re.match("^</(\w+)", line):
+                        if len(tabs) > 1:
+                            tabs = tabs[:-1]
+                        else:
+                            tabs = ""
+                    f.write("{0}{1}\n".format(tabs, line))
+
+            f.truncate()
+            f.close()
+        except Exception, e:
+            raise Exception("The changes have been made but there was an error occurred while formatting your file:\n{0}".format(e.message))
