7e4f8fe576aef946b2d1129a54f5e4fbb65529fc
Joe Gerew <joe.gerew@digicert.com>
2015-03-09 14:22:51 -0600

    Updated Augeas matching to use regular expressions.

--- digicert_express/parsers/base.py
+++ digicert_express/parsers/base.py
@@ -62,6 +62,7 @@ class BaseParser(object):
                 raise Exception("We could not find your main apache configuration file.  Please ensure that apache is "
                                 "running or include the path to your virtual host file in your command line arguments")
         except Exception, e:
+            self.check_for_parsing_errors()
             raise ParserException(
                 "An error occurred while loading your apache configuration.\n{1}".format(e.message),
                 self.directives)
@@ -95,7 +96,7 @@ class BaseParser(object):
         # get the augeas path to the config file
         apache_config = "/files{0}".format(apache_config)
 
-        incl_regex = "({0})|({1})".format('Include', 'IncludeOptional')
+        incl_regex = "({0})|({1})".format(create_regex('Include'), create_regex('IncludeOptional'))
         includes = self.aug.match(("{0}//* [self::directive=~regexp('{1}')]/* [label()='arg']".format(apache_config, incl_regex)))
 
         if includes:
@@ -145,8 +146,8 @@ class BaseParser(object):
         for match in matches:
             host_file = "/files{0}".format(self.aug.get(match))
             if '~previous' not in host_file:
-                vhosts = self.aug.match(host_file + "/VirtualHost")
-                vhosts += self.aug.match(host_file + "/*/VirtualHost")
+                vhosts = self.aug.match("{0}/*[label()=~regexp('{1}')]".format(host_file, create_regex("VirtualHost")))
+                vhosts += self.aug.match("{0}/*/*[label()=~regexp('{1}')]".format(host_file, create_regex("VirtualHost")))
 
                 vhost = self._get_vhost_path_by_domain_and_port(vhosts, '443')
                 if not vhost:
@@ -164,22 +165,15 @@ class BaseParser(object):
     def _get_vhost_path_by_domain_and_port(self, vhosts, port):
         for vhost in vhosts:
             if port in self.aug.get(vhost + "/arg"):
-                check_matches = self.aug.match(vhost + '/directive')
+                check_matches = self.aug.match("{0}/*[self::directive=~regexp('{1}')]".format(vhost, create_regex("ServerName")))
                 if check_matches:
                     for check in check_matches:
-                        if self.aug.get(check) == 'ServerName' and self.aug.get(check + "/arg") is not None \
-                                and self.aug.get(check + "/arg") == self.domain:
-                            return vhost
-                else:
-                    check_matches = self.aug.match(vhost + '/directive')
-                    for check in check_matches:
-                        if self.aug.get(check) == 'ServerName' and self.aug.get(check + "/arg") is not None \
-                                and self.aug.get(check + "/arg") == self.domain:
+                        if self.aug.get(check + "/arg") and self.aug.get(check + "/arg") == self.domain:
                             return vhost
 
     def _create_secure_vhost(self, vhost):
         secure_vhost = None
-        host_file = vhost[0:vhost.index("VirtualHost")-1]
+        host_file = "/files{0}".format(get_path_to_file(vhost))
 
         # create a map of the insecure vhost's configuration
         vhost_map = list()
@@ -187,7 +181,7 @@ class BaseParser(object):
 
         # check if there is an IfModule for mod_ssl.c, if not create it
         if_module = None
-        check_matches = self.aug.match(host_file + "/IfModule")
+        check_matches = self.aug.match("{0}/*[label()=~regexp('{1}')]".format(host_file, create_regex("IfModule")))
         if check_matches:
             for check in check_matches:
                 if self.aug.get(check + "/arg") == "mod_ssl.c":
@@ -195,19 +189,18 @@ class BaseParser(object):
 
         if not if_module:
             self.aug.set(host_file + "/IfModule[last()+1]/arg", "mod_ssl.c")
-            if_modules = self.aug.match("{0}/*[self::IfModule/arg='mod_ssl.c']".format(host_file))
+            if_modules = self.aug.match(host_file + "/*[self::IfModule/arg='mod_ssl.c']")
             if len(if_modules) > 0:
                 if_module = if_modules[0]
             else:
                 raise ParserException(
-                    "An error occurred while creating IfModule mod_ssl.c for {0}.\n{1}".format(self.domain, e.message),
-                    self.directives)
+                    "An error occurred while creating IfModule mod_ssl.c for {0}.".format(self.domain), self.directives)
 
         # create a new secure vhost
         vhost_name = self.aug.get(vhost + "/arg")
         vhost_name = vhost_name[0:vhost_name.index(":")] + ":443"
         self.aug.set(if_module + "/VirtualHost[last()+1]/arg", vhost_name)
-        vhosts = self.aug.match("{0}/IfModule/*[self::VirtualHost/arg='{1}']".format(host_file, vhost_name))
+        vhosts = self.aug.match("{0}/*[self::VirtualHost/arg='{1}']".format(if_module, vhost_name))
         for vhost in vhosts:
             secure_vhost = vhost
 
@@ -220,7 +213,7 @@ class BaseParser(object):
 
     def _create_map_from_vhost(self, path, vhost_map, text=""):
         # recurse through the directives and sub-groups to generate a map
-        check_matches = self.aug.match(path + '/*')
+        check_matches = self.aug.match(path + "/*")
         if check_matches:
             for check in check_matches:
                 values = list()
@@ -288,14 +281,15 @@ class BaseParser(object):
                 raise Exception("Virtual Host was not found for {0}".format(self.domain))
 
             # back up the configuration file
-            host_file = vhost_path[6:vhost_path.index("IfModule")-1]
+            host_file = get_path_to_file(vhost_path)
             shutil.copy(host_file, "{0}~previous".format(host_file))
 
             errors = []
             for directive in self.directives:
-                val = self.aug.match("{0}/*[self::directive='{1}']".format(vhost_path, directive))
-                if val:
-                    self.aug.set("{0}/*[self::directive='{1}']/arg".format(vhost_path, directive), self.directives[directive])
+                matches = self.aug.match("{0}/*[self::directive=~regexp('{1}')]".format(vhost_path, create_regex(directive)))
+                if len(matches) > 0:
+                    for match in matches:
+                        self.aug.set("{0}/arg".format(match), self.directives[directive])
                 else:
                     self.aug.set(vhost_path + "/directive[last()+1]", directive)
                     self.aug.set(vhost_path + "/directive[last()]/arg", self.directives[directive])
@@ -314,7 +308,12 @@ class BaseParser(object):
             # verify the added/modified directives are the values we set
             errors = []
             for directive in self.directives:
-                val = self.aug.get("{0}/*[self::directive='{1}']/arg".format(vhost_path, directive))
+                val = None
+                matches = self.aug.match("{0}/*[self::directive=~regexp('{1}')]/arg".format(vhost_path, create_regex(directive)))
+                if len(matches) > 0:
+                    for match in matches:
+                        val = self.aug.get(match)
+
                 if val != self.directives[directive]:
                     errors.append("{0} is {1} instead of {2}".format(directive, val, self.directives[directive]))
 
@@ -331,47 +330,24 @@ class BaseParser(object):
                 self.directives)
 
         # format the file:
-        self._format_config_file(host_file)
-
-        # verify that augeas can still load the changed file
-        self.load_apache_configs()
-
-    @staticmethod
-    def _format_config_file(host_file):
         try:
-            # get the lines of the config file
-            lines = list()
-            with open(host_file) as f:
-                lines = f.read().splitlines()
-
-            f = open(host_file, 'w+')
-
-            try:
-                tabs = ""
-                for line in lines:
-                    line = line.lstrip()
-                    # check for the beginning of a tag, if found increase the indentation after writing the tag
-                    if re.match("^<(\w+)", line):
-                        f.write("{0}{1}\n".format(tabs, line))
-                        tabs += "\t"
-                    else:
-                        # check for the end of a tag, if found decrease the indentation
-                        if re.match("^</(\w+)", line):
-                            if len(tabs) > 1:
-                                tabs = tabs[:-1]
-                            else:
-                                tabs = ""
-                        # write the config/tag
-                        f.write("{0}{1}\n".format(tabs, line))
-            finally:
-                f.truncate()
-                f.close()
+            format_config_file(host_file)
         except Exception, e:
             raise Exception("The changes have been made but there was an error occurred while formatting "
                             "your file:\n{0}".format(e.message))
 
+        # verify that augeas can still load the changed file
+        self.aug.load()
+
 
 def verify_file(file_path):
+    """
+    Verify that the file exists and the user has the necessary permissions to this file
+
+    :param file_path
+    :return:
+    """
+
     if not os.path.isfile(file_path):
         raise ParserException("{0} could not be found on the filesystem".format(file_path))
 
@@ -379,3 +355,73 @@ def verify_file(file_path):
     if file_perm != 755:
         raise ParserException("{0} does not have the necessary permissions set "
                               "(755 required, {1} set)".format(file_path, file_perm))
+
+def get_path_to_file(path):
+    """
+    Take an augeas path (ie: /files/etc/apache2/apache2.conf/VirtualHost/Directory/) and return the path
+    to the apache configuration file (ie: /etc/apache2/apache2.conf)
+
+    :param path
+    :return: path to an actual file or None
+    """
+
+    if "/files/" in path[:7]:
+        path = path[6:]
+
+    while not os.path.exists(path) and not os.path.isdir(path):
+        last_slash_index = path.rfind("/")
+        if last_slash_index > 0:
+            path = path[:last_slash_index]
+        else:
+            return None
+    return path
+
+
+def create_regex(text):
+    """
+    Escape and return the passed string in upper and lower case to match regardless of case.
+    Augeas 1.0 supports the standard regex /i but previous versions do not.  Also, not all (but most) unix/linux
+    platforms support /i.  So this is the safest method to ensure matches.
+
+    :param text: string to create regex from
+    :return: regex
+    """
+
+    return "".join(["[" + c.upper() + c.lower() + "]" if c.isalpha() else c for c in re.escape(text)])
+
+
+def format_config_file(host_file):
+    """
+    Format the apache configuration file.  Loop through the lines of the file and indent/un-indent where necessary
+
+    :param host_file:
+    :return:
+    """
+
+    # get the lines of the config file
+    lines = list()
+    with open(host_file) as f:
+        lines = f.read().splitlines()
+
+    f = open(host_file, 'w+')
+
+    try:
+        tabs = ""
+        for line in lines:
+            line = line.lstrip()
+            # check for the beginning of a tag, if found increase the indentation after writing the tag
+            if re.match("^<(\w+)", line):
+                f.write("{0}{1}\n".format(tabs, line))
+                tabs += "\t"
+            else:
+                # check for the end of a tag, if found decrease the indentation
+                if re.match("^</(\w+)", line):
+                    if len(tabs) > 1:
+                        tabs = tabs[:-1]
+                    else:
+                        tabs = ""
+                # write the config/tag
+                f.write("{0}{1}\n".format(tabs, line))
+    finally:
+        f.truncate()
+        f.close()
