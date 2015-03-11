6a9b6da2ce7877ea001f70511a4b87e6baa05c2d
Jeff Fischer <jeff.fischer@digicert.com>
2015-03-11 12:33:03 -0600

    install script changes and fixes for download functionality and cleanup

--- digicert_express/scripts/install.sh
+++ digicert_express/scripts/install.sh
@@ -1,110 +1,75 @@
 #!/bin/sh
 
-AUG_X64="augeas-lenses augeas-tools libaugeas0 python-augeas"
-AUG_X386="augeas-lenses augeas-tools:i386 libaugeas0:i386 python-augeas"
-
-DIGICERT_INSTALL="digicert_procure express_install"
+INSTALL_PACKAGES=""
+DIGICERT_PACKAGES="digicert_client express_install"
 
 # check for distribution, debian, centos, ubuntu
-check_distribution() {
-    if [ -f /etc/lsb-release ]; then
-            os=$(lsb_release -s -d)
-    elif [ -f /etc/debian_version ]; then
-            os="Debian $(cat /etc/debian_version)"
-    elif [ -f /etc/centos-release ]; then
-            os=`cat /etc/redhat-release`
-    else
-            os="$(uname -s) $(uname -r)"
-    fi
-
-    a=( $os )
-    echo ${a[0]}
-}
+if [ -f /etc/lsb-release ]
+then
+        os=$(lsb_release -s -d)
+elif [ -f /etc/debian_version ]; then
+        os="Debian $(cat /etc/debian_version)"
+elif [ -f /etc/centos-release ]; then
+        os=`cat /etc/redhat-release`
+else
+        os="$(uname -s) $(uname -r)"
+fi
+echo $os
 
 
 
 # check for architecture 32 bit or 64 bit
-check_architecture() {
-    MACHINE_TYPE=`uname -m`
-    if [ ${MACHINE_TYPE} == 'x86_64' ]; then
-        # 64-bit stuff here
-        MISC=$AUG_X64
-    else
-      # 32-bit stuff here
-        MISC=$AUG_X386
-    fi
-    local myresult="x64"
-    echo $MACHINE_TYPE
-}
+MACHINE_TYPE=`uname -m`
+if [ ${MACHINE_TYPE} = "x86_64" ]
+then
+    INSTALL_PACKAGES="augeas-lenses augeas-tools libaugeas0 python-augeas openssl python-pip"
+else
+    INSTALL_PACKAGES="augeas-lenses augeas-tools:i386 libaugeas0:i386 python-augeas openssl python-pip"
+fi
 
 
 
-# check for augeas packages
-check_for_augeas() {
-
-    for pkg in $MISC; do
-          if dpkg --get-selections | grep -q "^$pkg[[:space:]]*install$" >/dev/null; then
-              echo -e "$pkg is already installed"
+if [[ $os == *"CentOS"* ]]
+then
+    for package in "openssl augeas-libs augeas python-pip"; do
+        if yum list installed "$package" >/dev/null 2>&1
+        then
+            echo "$package is already installed"
+        else
+            echo "$package is not installed"
+            echo "$package needs to be installed"
+            echo "Should I install $package (y/n)"
+            read REPLY
+            if [ "$REPLY" = "y" ]
+            then
+                sudo yum -q install $package
+                echo "Successfully installed $package"
+            fi
+        fi
+    done
+else
+    for pkg in $INSTALL_PACKAGES
+    do
+          if dpkg --get-selections | grep -q "^$pkg[[:space:]]*install$" >/dev/null
+          then
+              echo "$pkg is already installed"
           else
               echo "$pkg is not installed"
               echo "$pkg needs to be installed"
-              echo -e "Should I install $pkg (y/n) \c"
+              echo "Should I install $pkg (y/n)"
               read
-              if "$REPLY" = "y"; then
+              if "$REPLY" = "y"
+              then
                   apt-get -q install $pkg
                   echo "Successfully installed $pkg"
               fi
           fi
     done
-}
-
-
-check_for_augeas_centos() {
-    "yum list installed bind"
-
-    for pkg in $MISC; do
-          if yum list installed "$package" >/dev/null 2>&1; then
-              echo -e "$pkg is already installed"
-          else
-              echo "$pkg is not installed"
-              echo "$pkg needs to be installed"
-              echo -e "Should I install $pkg (y/n) \c"
-              read
-              if "$REPLY" = "y"; then
-                  yum -q install $pkg
-                  echo "Successfully installed $pkg"
-              fi
-          fi
-    done
-}
-
-# install our deps via pip
-check_for_digicert_deps() {
-    for pkg in $DIGICERT_INSTALL; do
-          pip install $pkg
-          echo "Successfully installed $pkg"
-    done
-}
-
-
-
-# 1.  call check_architecture
-arch=$(check_architecture)
-echo $arch
-
-# 2.  call check_distro
-distro=$(check_distribution)
-echo $distro
-
-# 3.  check and install packages
-if "$distro"= 'CentOS'; then
-    check_for_augeas_centos
-else
-    check_for_augeas
 fi
 
-# 4.  install express install client
-check_for_digicert_deps
-
-#res=$(testReturn)
-#echo $res
+# install digicert modules
+for package in $DIGICERT_PACKAGES
+do
+      sudo pip install $package
+      echo "Successfully installed $package"
+done
--- digicert_express/scripts/test.sh
+++ digicert_express/scripts/test.sh
@@ -1,73 +1,74 @@
 #!/bin/sh
 
-AUG_X64="augeas-lenses augeas-tools libaugeas0 python-augeas"
-AUG_X386="augeas-lenses augeas-tools:i386 libaugeas0:i386 python-augeas"
+INSTALL_PACKAGES=""
+DIGICERT_PACKAGES="digicert_client express_install"
 
-
-# detect if 64 or 32 bit machine because packages are different
-MACHINE_TYPE=`uname -m`
-if [ ${MACHINE_TYPE} == 'x86_64' ]; then
-    # 64-bit stuff here
-   MISC=$AUG_X64
+# check for distribution, debian, centos, ubuntu
+if [ -f /etc/lsb-release ]
+then
+        os=$(lsb_release -s -d)
+elif [ -f /etc/debian_version ]; then
+        os="Debian $(cat /etc/debian_version)"
+elif [ -f /etc/centos-release ]; then
+        os=`cat /etc/redhat-release`
 else
-      # 32-bit stuff here
-   MISC=$AUG_X386
+        os="$(uname -s) $(uname -r)"
+fi
+echo $os
+
+
+
+# check for architecture 32 bit or 64 bit
+MACHINE_TYPE=`uname -m`
+if [ ${MACHINE_TYPE} = "x86_64" ]; then
+    # 64-bit stuff here
+    INSTALL_PACKAGES="augeas-lenses augeas-tools libaugeas0 python-augeas openssl"
+else
+  # 32-bit stuff here
+    INSTALL_PACKAGES="augeas-lenses augeas-tools:i386 libaugeas0:i386 python-augeas openssl"
 fi
 
+
+echo ${INSTALL_PACKAGES}
 echo ${MACHINE_TYPE}
-echo ${MISC}
+echo ${os}
 
-for pkg in $MISC; do
-      if dpkg --get-selections | grep -q "^$pkg[[:space:]]*install$" >/dev/null; then
-          echo -e "$pkg is already installed"
-      else
-          echo "$pkg is not installed"
-          echo "$pkg needs to be installed"
-          echo -e "Should I install $pkg (y/n) \c"
-          read
-          if "$REPLY" = "y"; then
-              apt-get -q install $pkg
-              echo "Successfully installed $pkg"
+
+if [[ $os == *"CentOS"* ]]
+then
+    for package in "openssl augeas-libs augeas python-pip"; do
+        if yum list installed "$package" >/dev/null 2>&1; then
+            echo "$package is already installed"
+        else
+            echo "$package is not installed"
+            echo "$package needs to be installed"
+            echo "Should I install $package (y/n)"
+            read REPLY
+            if [ "$REPLY" = "y" ]; then
+                sudo yum -q install $package
+                echo "Successfully installed $package"
+            fi
+        fi
+    done
+else
+    for pkg in $INSTALL_PACKAGES; do
+          if dpkg --get-selections | grep -q "^$pkg[[:space:]]*install$" >/dev/null; then
+              echo "$pkg is already installed"
+          else
+              echo "$pkg is not installed"
+              echo "$pkg needs to be installed"
+              echo "Should I install $pkg (y/n)"
+              read
+              if "$REPLY" = "y"; then
+                  apt-get -q install $pkg
+                  echo "Successfully installed $pkg"
+              fi
           fi
-      fi
+    done
+fi
+
+# install digicert modules
+for package in $DIGICERT_PACKAGES; do
+      sudo pip install $package
+      echo "Successfully installed $package"
 done
-
-
-
-!#/bin/bash
-
-AUG_X64="augeas-lenses augeas-tools libaugeas0 python-augeas"
-AUG_X386="augeas-lenses augeas-tools:i386 libaugeas0:i386 python-augeas"
-
-
-# detect if 64 or 32 bit machine because packages are different
-MACHINE_TYPE=`uname -m`
-if [ ${MACHINE_TYPE} == 'x86_64' ]; then
-      # 64-bit stuff here
-         MISC=$AUG_X64
-       else
-               # 32-bit stuff here
-                  MISC=$AUG_X386
-                fi
-                echo "hello"
-                echo ${MACHINE_TYPE}
-                echo ${MISC}
-
-
-                if [ -f /etc/lsb-release ]; then
-                          os=$(lsb_release -s -d)
-                        elif [ -f /etc/debian_version ]; then
-                                  os="Debian $(cat /etc/debian_version)"
-                                elif [ -f /etc/centos-release ]; then
-                                          os=`cat /etc/redhat-release`
-                                        else
-                                                  os="$(uname -s) $(uname -r)"
-                                                fi
-
-                                                a=( $os )
-                                                echo ${a[0]}
-
-                                                if [ ${a[0]} == 'CentOS' ]; then
-                                                      echo 'success'
-                                                    fi
-
