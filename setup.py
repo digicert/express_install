5bb57d66774d9fe046c835294cdb180d4d0b48b0
Matt Ryan <matt.ryan@digicert.com>
2015-03-24 17:40:53 -0600

    Release 1.0.0b2

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def readme():
 
 setup(
     name='digicert_express',
-    version='1.0.0b1',
+    version='1.0.0b2',
     description='Express Install for DigiCert, Inc.',
     long_description=readme(),
     classifiers=[
