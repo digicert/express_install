a27a306029f5af69293ddc82fecd2e26ca630e53
Jeff Fischer <jeff.fischer@digicert.com>
2015-05-06 09:46:03 -0600

    backing out logging changes for later

--- digicert_express/parsers/base.py
+++ digicert_express/parsers/base.py
@@ -31,7 +31,7 @@ class BaseParser(object):
     def __init__(self, domain, cert_path, key_path, chain_path, storage_path='/etc/digicert',
                  aug=None, logger=None, dry_run=False):
         if not logger:
-            self.logger = ExpressInstallLogger().get_logger()
+            logger = ExpressInstallLogger().get_logger()
 
         self.domain = domain
         self.dry_run = dry_run
@@ -53,11 +53,11 @@ class BaseParser(object):
         # verify that the files exist and are readable by the user
         # TODO: logger as a class member doesn't need to be passed in
         cert_path = verify_and_normalize_file(cert_path, "Certificate file", domain.replace(".", "_") + ".crt",
-                                              apache_user, storage_path, self.logger, dry_run, keep_original = False)
+                                              apache_user, storage_path, logger, dry_run, keep_original = False)
         chain_path = verify_and_normalize_file(chain_path, "CA Chain file", "DigiCertCA.crt",
-                                               apache_user, storage_path, self.logger, dry_run, keep_original = False)
+                                               apache_user, storage_path, logger, dry_run, keep_original = False)
         key_path = verify_and_normalize_file(key_path, "Key file", domain.replace(".", "_") + ".key",
-                                             apache_user, storage_path, self.logger, dry_run, keep_original = True)
+                                             apache_user, storage_path, logger, dry_run, keep_original = True)
 
         self.directives = OrderedDict()
         self.directives['SSLEngine'] = "on"
@@ -364,8 +364,6 @@ class BaseParser(object):
             # back up the configuration file
             host_file = get_path_to_file(vhost_path)
             shutil.copy(host_file, "{0}~previous".format(host_file))
-
-            self.logger.info("Updating configuration in virtual host configuration: %s" % host_file)
 
             errors = []
             if self.dry_run:
