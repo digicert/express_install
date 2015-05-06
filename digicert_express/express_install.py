60bc60be23e21a45f415a06c0ff21b99ba5ef91d
Jeff Fischer <jeff.fischer@digicert.com>
2015-05-06 17:09:28 -0600

    bumping for release

--- digicert_express/express_install.py
+++ digicert_express/express_install.py
@@ -37,7 +37,7 @@ DEB_DEPS_32 = ['augeas-lenses', 'augeas-tools:i386', 'libaugeas0:i386', 'openssl
 
 RH_DEPS = ['openssl', 'augeas-libs', 'augeas', 'mod_ssl']
 
-HOST = 'localhost.digicert.com'
+HOST = 'www.digicert.com'
 
 API_KEY = None
 
