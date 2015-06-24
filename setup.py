62a7f0397ad5c4fcf835c136890d6af728755126
Jeff Fischer <jeff.fischer@digicert.com>
2015-06-24 13:07:21 -0600

    bumping version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def readme():
 
 setup(
     name='digicert_express',
-    version='1.1.01',
+    version='1.1.02',
     description='Express Install for DigiCert, Inc.',
     long_description=readme(),
     classifiers=[
