8d2d5e8ed786799d89bacebad6ffbb89677fa979
Jeff Fischer <jeff.fischer@digicert.com>
2015-06-30 16:24:52 -0600

    new version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def readme():
 
 setup(
     name='digicert_express',
-    version='1.1.31',
+    version='1.1.32',
     description='Express Install for DigiCert, Inc.',
     long_description=readme(),
     classifiers=[
